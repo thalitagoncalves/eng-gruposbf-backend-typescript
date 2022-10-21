500880b797f1c603e7d7ad6810f5e1238e897d20
thali <thagsousa15@gmail.com>
2022-10-21 15:10:47 -0300

    feat: add middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import CoinConvert from './domains/currency';
+import convertMiddleware from './middleware';
 
 const router = Router();
 
-router.get('/convert/:amount', async (req, res) => {
+router.get('/convert/:amount', convertMiddleware, async (req, res) => {
   await CoinConvert.convert(res, { amount: req.params.amount });
 });
 
