a701779400873c764832724ffadcfa7785f6581f
Thalita Gonçalves <61715464+thalitagoncalves@users.noreply.github.com>
2022-10-22 16:54:11 -0300

    fix structure and tests (#5)
    * chore: add .gitignore
    
    * refactor: change code to support other currencies
    
    * feat: add file with currencies
    
    * feat: add middleware to handle errors
    
    * feat: add middleware
    
    * refactor: change code to support other currencies
    
    * refactor: create function just to get rates
    
    * refactor: handle response only on routes.ts
    
    * feat: add middleware to handle with erros in the request
    
    * test: finish converter tests
    
    * refactor: add a folder to types
    
    * refactor: replace bodyParser to urlencoded

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import router from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
 
 app.use('/', router);
 
