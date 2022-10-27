1defa203216402d2ad0df7f375aea4fc3956cd46
thali <thagsousa15@gmail.com>
2022-10-27 14:41:53 -0300

    refactor: remove unnecessary currencies

--- src/config/currencies.ts
+++ src/config/currencies.ts
@@ -1,10 +1,5 @@
 export default {
-  AUD: 'Dólar Australiano',
   EUR: 'Euro',
-  ARS: 'Peso Argentino',
   INR: 'Rúpia Indiana',
   USD: 'Dólar Americano',
-  CLP: 'Peso Chileno',
-  GBP: 'Libra Esterlina',
-  MXN: 'Peso Mexicano',
 };
