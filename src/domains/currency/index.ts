8d934065b25704b558e95e339cbe966f0b155656
Thali Gonçalves <61715464+thalitagoncalves@users.noreply.github.com>
2022-10-27 14:44:04 -0300

    reefactor: quick fixes (#9)
    * refactor: remove port of domain and add it at src
    
    * refactor: remove unnecessary currencies
    
    * refactor: change currency port import
    
    * refactor: remove port of domain

--- src/domains/currency/index.ts
+++ src/domains/currency/index.ts
@@ -1,4 +1,4 @@
 import _convert from './converter';
-import GetQuotation from './ports/coins-api/index';
+import GetQuotation from '../../ports/coins-api/index';
 
 export default { convert: _convert(GetQuotation) };
--- src/ports/coins-api/index.ts
+++ src/ports/coins-api/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import currencies from '../../../../config/currencies';
+import currencies from '../../config/currencies';
 
 import { IQuotationApiResponse } from './types';
 
